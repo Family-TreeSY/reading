8ceaa6d3f144bce98b797c7be1ea8dfc477f71ff
Family-TreeSY <286210002@qq.com>
2018-06-13 19:46:57 +0800

    '创建Django自带的admin后台管理平台'

--- reading/book/admin.py
+++ reading/book/admin.py
@@ -1,3 +1,43 @@
+# -*- coding:utf-8 -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Story
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'status',
+        'user',
+        'created_time',
+    )
+
+
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'author',
+        'user',
+        'created_time'
+    )
+    list_filter = (
+        'author',
+    )
+    search_fields = [
+        'name',
+        'category__name',
+        'author',
+    ]
+    date_hierarchy = 'created_time'
+    fields = (
+        'name',
+        ('image', 'category'),
+        ('author', 'user'),
+        'desc',
+        'content',
+    )
