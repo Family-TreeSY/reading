55dbc5c87a50e88b5a2e6ba044cf41eef0b7b0df
Family-TreeSY <286210002@qq.com>
2018-06-29 21:47:29 +0800

    '给category增加文章列表'

--- reading/book/api.py
+++ reading/book/api.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from rest_framework import serializers, viewsets
 
-from .models import Story
+from .models import Story, Category
 
 
 class StorySerializer(serializers.ModelSerializer):
@@ -25,7 +25,24 @@ class StorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Story
         fields = (
-            'name', 'status', 'category', 'author', 'user', 'created_time',
+            'url', 'name', 'status', 'category', 'author', 'user', 'created_time',
+        )
+
+
+class StoryDetailSerializer(serializers.ModelSerializer):
+    stories = StorySerializer(
+        read_only=True,
+    )
+
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+    )
+
+    class Meta:
+        model = Story
+        fields = (
+            'id', 'name', 'user', 'desc', 'stories',
         )
 
 
@@ -33,13 +50,12 @@ class StoryViewSet(viewsets.ModelViewSet):
     queryset = Story.objects.all()
     serializer_class = StorySerializer
 
+    def retrieve(self, request, *args, **kwargs):
+        self.serializer_class = StoryDetailSerializer
+        return super(StoryViewSet, self).retrieve(request, *args, **kwargs)
+
 
 class UserSerializer(serializers.ModelSerializer):
-    # user = serializers.SlugRelatedField(
-    #     slug_field='username',
-    #     read_only=True,
-    # )
-
     class Meta:
         model = User
         fields = (
@@ -50,3 +66,47 @@ class UserSerializer(serializers.ModelSerializer):
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.filter(is_staff=True)
     serializers_class = UserSerializer
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+    )
+
+    created_time = serializers.DateTimeField(
+        format="%Y-%m-%d %H:%M:%S",
+    )
+
+    class Meta:
+        model = Category
+        fields = (
+            'url', 'name', 'status', 'is_nav', 'user', 'created_time',
+        )
+
+
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    story_set = StorySerializer(
+        many=True,
+        read_only=True,
+    )
+
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+    )
+
+    class Meta:
+        model = Category
+        fields = (
+            'id', 'name', 'user', 'story_set',
+        )
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.filter(status=1)
+    serializer_class = CategorySerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        self.serializer_class = CategoryDetailSerializer
+        return super(CategoryViewSet, self).retrieve(request, *args, **kwargs)
